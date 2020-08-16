3696f9e4d4196c744462570911e9b32ec8b23d26
acampbell4 <asha.i.campbell@gmail.com>
2020-08-16 04:29:43 -0400

    PaintBall Color loop created

--- Assets/Scripts/PaintBallPellet.cs
+++ Assets/Scripts/PaintBallPellet.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class PaintBallPellet : MonoBehaviour
 {
     public List<Material> ourPaints = new List<Material>();
+    private int paintIndex = 0;
    private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag =="Paintable")       //== means you are compairing two = means equal
         {
             //do the painting
+            collision.collider.GetComponent<Renderer>().material = ourPaints[paintIndex];
+
+            paintIndex++;
+            if(paintIndex == ourPaints.Count)
+            {
+                paintIndex = 0;
+            }
         }
     }
 }
