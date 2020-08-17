694049d7f0580bd9604c77029d6752b4cf805def
acampbell4 <asha.i.campbell@gmail.com>
2020-08-17 08:55:02 -0400

    Revert "Moved the jump and  down script to bottom again."
    This reverts commit 2928cc6940b1a77062a221e2fd279043005fe498.

--- Assets/Scripts/SImHandMove.cs
+++ Assets/Scripts/SImHandMove.cs
@@ -47,7 +47,16 @@ public class SImHandMove : MonoBehaviour
         #endregion
         //mouse stuff//
         #region translational movement
-     
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * moveSpeed);
+        }
+
+
         if (Input.GetKey(KeyCode.W))
         {
             this.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
@@ -72,15 +81,6 @@ public class SImHandMove : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             moveSpeed /= sprint;
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
-        }
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            transform.Translate(Vector3.down * Time.deltaTime * moveSpeed);
         }
         #endregion
         #region rotation using mouse
