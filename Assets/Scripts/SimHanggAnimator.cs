960c06ffd968143fd73cbe9ad73ada8b84eef054
acampbell4 <asha.i.campbell@gmail.com>
2020-08-18 19:35:55 -0400

    Hand Grab works for SimHand

---
+++ Assets/Scripts/SimHanggAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimHanggAnimator : MonoBehaviour
+{
+    private Animator simHandAnim;
+
+    void Start()
+    {
+        simHandAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            simHandAnim.SetBool("IsClosing", true);  //closing the fist 
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            simHandAnim.SetBool("IsClosing", false); //opening fist
+        }
+    }
+}
