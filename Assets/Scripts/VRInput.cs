5b88100f55b3f57421f59b9bcaf42626a83da384
acampbell4 <asha.i.campbell@gmail.com>
2020-08-18 19:59:51 -0400

    Added  VRInputs for Grips

---
+++ Assets/Scripts/VRInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRInput : MonoBehaviour
+{
+    public bool isLeftHand; //if true,left controller
+    public float gripValue;
+
+    private string gripAxis;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(isleftHand)
+        {
+            gripAxis = "LeftGrip";
+        }
+        else
+        {
+            gripAxis = "RightGrip";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gripValue = Input.GetAxis(gripAxis); 
+    }
+}
